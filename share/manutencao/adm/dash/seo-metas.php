399b446223994600273d01dd7ef5626d7bb75420
usinateccom <comercial@tecnologiacomercialbh.com.br>
2018-02-05 17:08:37 -0200

    pastas adm/ e manutencao/ somente com conteudo estático. funcoes dinamicas serão revisadas e colocadas via inclue (apenas para atualização de arquvos - editamos mais tarde)

---
+++ share/manutencao/adm/dash/seo-metas.php
@@ -0,0 +1,119 @@
+<?php
+
+include "../../../config/config.php";
+
+include "../../../funcoes/funcoes.php";
+
+?>
+<div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Metatags e Conteúdo</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+
+
+            <div class="row" id='canja'>
+<div class="cinquent esq_f" style="width: 50%; display: inline-table;float: left;">
+
+
+<div class="grifado" style="border-bottom: 1px solid #ccc; margin-bottom: 30px;">
+
+<h2>Páginas</h2>
+  </div>
+                <!-- /.col-lg-12 -->
+
+
+
+
+
+
+<?php
+
+$dokun = sel_simples("paginas_hj", "*", "");
+
+while ($fon = mysql_fetch_array($dokun)) {
+	# code...
+
+$dark = $fon["urlen"];
+
+if($dark == ""){
+
+$dark = '-';
+
+}
+
+	$arraylandia = $fon["titulo"]."::".$fon["k_words"]."::".$fon["descric"]."::".$dark."::".$fon["hu"]."::".$fon["alte"].'::'.$fon["altura"].'::'.$fon["qual_e"];
+
+
+echo "  <a href='#' onclick=".'"'."loa('$arraylandia')".'"'." class='pulim'><i class='fa fa-table fa-fw'></i> ".cresce($fon["qual_e"])."</a><br>";
+
+
+
+
+} // while
+
+?>
+
+<br><br><button class='btn btn-primary' id='steie' onclick='bolacha()'>INSERIR META-TAGS</button>
+
+
+</div><!--.esf-->
+
+
+<div class="cinquent dir_f" id="cont_dir" style="width: 50%; display: inline-table;float: right;">
+<dic class='reguad' id='condcim'>
+
+</dic>
+
+
+<iframe class="dnone" id="condbax">
+
+
+
+</iframe>
+
+
+
+
+</div><!--dir-->
+
+
+
+
+            </div><!--canja-->
+
+
+<script>
+
+	function loa(array){
+
+
+var domu = array.split("::");
+
+//alert('foi');
+
+var varu = "<h3> Atualizar Metas</h3><h4>Titulo</h4>  <input type='text' id='titulo' value='"+domu[0]+"' class='form-control'><h4>K-Words</h4> <input type='text' id='kword' class='form-control'  value='"+domu[1]+"'> <h4>Descrição</h4> <input type='text' id='descric' class='form-control'  value='"+domu[2]+"'> <h4>Url</h4> <input type='text' id='urle' class='form-control'  value='"+domu[3]+"'> <h4>H1</h4> <input type='text' id='hu' class='form-control'  value='"+domu[4]+"'>   <h4>AltTag</h4> <input type='text' id='alte' class='form-control'  value='"+domu[5]+"'> <h4>Nível</h4> <input type='text' id='nivel' class='form-control'  value='"+domu[6]+"'><h4>Nome da Página</h4> <input type='text' id='quale' class='form-control'  value='"+domu[7]+"'> <br><br><span> <button class='btn btn-defaut' id='cantanon'>ATUALIZAR</button></span><span id='vumora'  style='margin-left:30px;'></span>";
+
+$("#condcim").html(varu);
+
+$("#condbax").attr('src','partials/botao.html');
+
+	}
+
+
+function bolacha(){
+
+
+
+var varu = "<h3> Inserir Metas</h3><h4>Titulo</h4>  <input type='text' id='titulo' value='' class='form-control'><h4>K-Words</h4> <input type='text' id='kword' class='form-control'  value=''> <h4>Descrição</h4> <input type='text' id='descric' class='form-control'  value=''> <h4>Url</h4> <input type='text' id='urle' class='form-control'  value=''> <h4>H1</h4> <input type='text' id='hu' class='form-control'  value=''>   <h4>AltTag</h4> <input type='text' id='alte' class='form-control'  value=''> <h4>Nível</h4> <input type='text' id='nivel' class='form-control'  value=''><h4>Nome da Página</h4> <input type='text' id='quale' class='form-control'  value=''> <br><br><span> <button class='btn btn-defaut' id='cantanon'>ATUALIZAR</button></span><span id='vumora'  style='margin-left:30px;'></span>";
+
+$("#condcim").html(varu);
+
+$("#condbax").attr('src','partials/botaob.html');
+
+
+}
+
+
+</script>
---
+++ share/manutencao/adm/index.php
@@ -0,0 +1,53 @@
+
+<?php
+
+$bot = "";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+	
+
+$bot = $_POST['belgo'];
+
+
+} else{
+
+
+header("location: http://www.instashare.me/manutencao/");
+
+}// post
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Manutenção Instashare</title>
+
+
+<script>
+function suba(){
+
+document.getElementById("fuji").submit();
+
+}
+</script>
+
+</head>
+<body onload="suba()">
+<div style="position: fixed; top: 50%; left: 50%; width: 150px; margin-left: -75px; color: #000; font-size: 14px; font-weight: bold;">
+<center>AGUARDE...</center></div>
+
+
+
+</div>
+
+
+<form action='dash/' method="post" style="display: none;" id='fuji'>
+	
+<input type="text" name="belgo" id='belgo' value="<?php echo $bot; ?>">
+
+</form>
+</body>
+</html>
